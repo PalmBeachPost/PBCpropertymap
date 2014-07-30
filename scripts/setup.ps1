b9d1cb7e4a078acc426e57358cf2a4563da17c81
unknown <cpersaud@NWPBPBP0DPC3211.cmg.int>
2014-07-30 10:59:02 -0400

    Updated .psl and .sql scripts
    -setup.psl makes CSV files out of 2012 and 2013 property data, and puts
    the data into PostgreSQL data tables
    -Both .sql scripts create the PostgreSQL tables and import the CSV files
    into them

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -6,15 +6,24 @@ param(
     )
 
 # convert all the fixedwidths to CSVs first
-.\Convert-fixedwidth-to-csv.ps1 -fwfile '..\valuedata\REC10_TRIM2013\*.TXT' -formatfile '.\formatfiles\REC10.csv'
-.\Convert-fixedwidth-to-csv.ps1 -fwfile '..\valuedata\REC20_TRIM2013\*.TXT' -formatfile '.\formatfiles\REC20.csv'
-.\Convert-fixedwidth-to-csv.ps1 -fwfile '..\valuedata\REC35_TRIM2013\*.TXT' -formatfile '.\formatfiles\REC35.csv'
-.\Convert-fixedwidth-to-csv.ps1 -fwfile '..\valuedata\REC36_TRIM2013\*.TXT' -formatfile '.\formatfiles\REC36.csv'
-.\Convert-fixedwidth-to-csv.ps1 -fwfile '..\valuedata\REC40_TRIM2013\*.TXT' -formatfile '.\formatfiles\REC40.csv'
+.\Convert-fixedwidth-to-csv.ps1 -fwfile '..\valuedata\2013\REC10_CERT2013.TXT' -formatfile '.\formatfiles\REC10.csv'
+.\Convert-fixedwidth-to-csv.ps1 -fwfile '..\valuedata\2013\REC20_CERT2013.TXT' -formatfile '.\formatfiles\REC20.csv'
+.\Convert-fixedwidth-to-csv.ps1 -fwfile '..\valuedata\2013\REC35_CERT2013.TXT' -formatfile '.\formatfiles\REC35.csv'
+.\Convert-fixedwidth-to-csv.ps1 -fwfile '..\valuedata\2013\REC36_COPY.TXT' -formatfile '.\formatfiles\REC36.csv'
+.\Convert-fixedwidth-to-csv.ps1 -fwfile '..\valuedata\2013\REC40_CERT2013.TXT' -formatfile '.\formatfiles\REC40.csv'
+.\Convert-fixedwidth-to-csv.ps1 -fwfile '..\valuedata\2012\REC10_TRIM2012.TXT' -formatfile '.\formatfiles\REC10.csv'
+.\Convert-fixedwidth-to-csv.ps1 -fwfile '..\valuedata\2012\REC20_TRIM2012.TXT' -formatfile '.\formatfiles\REC20.csv'
+.\Convert-fixedwidth-to-csv.ps1 -fwfile '..\valuedata\2012\REC35_TRIM2012.TXT' -formatfile '.\formatfiles\REC35.csv'
+.\Convert-fixedwidth-to-csv.ps1 -fwfile '..\valuedata\2012\REC36_COPY.TXT' -formatfile '.\formatfiles\REC36.csv'
+.\Convert-fixedwidth-to-csv.ps1 -fwfile '..\valuedata\2012\REC40_TRIM2012.TXT' -formatfile '.\formatfiles\REC40.csv'
 
 # Nuke db and start from scratch if specified
 if ($nuke -eq 1)
 {
+    # Fesh start, create the database
+    PSQL -U $u -h localhost -p $p -w -c "drop database if exists $db"
+    CREATEDB -U $u $db
+
     # create tables
     PSQL -U $u -h localhost -p $p -w -d $db -f ./sql/createTables.sql
     
@@ -24,3 +33,13 @@ if ($nuke -eq 1)
 }
 
 # Chris TODO: Now add script to import these files. Avoid hardcoding anywhere else except this one file
+PSQL -U $u -h localhost -p $p -w -d $db -c "COPY raw13Rec10 FROM '..\valuedata\2013\REC10_CERT2013.CSV' DELIMITER ','"
+PSQL -U $u -h localhost -p $p -w -d $db -c "COPY raw13Rec20 FROM '..\valuedata\2013\REC20_CERT2013.CSV' DELIMITER ','"
+PSQL -U $u -h localhost -p $p -w -d $db -c "COPY raw13Rec35 FROM '..\valuedata\2013\REC35_CERT2013.CSV' DELIMITER ','"
+PSQL -U $u -h localhost -p $p -w -d $db -c "COPY raw13Rec36 FROM '..\valuedata\2013\REC36_COPY.CSV' DELIMITER ','"
+PSQL -U $u -h localhost -p $p -w -d $db -c "COPY raw13Rec40 FROM '..\valuedata\2013\REC40_CERT2013.CSV' DELIMITER ','"
+PSQL -U $u -h localhost -p $p -w -d $db -c "COPY raw12Rec10 FROM '..\valuedata\2012\REC10_TRIM2012.CSV' DELIMITER ','"
+PSQL -U $u -h localhost -p $p -w -d $db -c "COPY raw12Rec20 FROM '..\valuedata\2012\REC20_TRIM2012.CSV' DELIMITER ','"
+PSQL -U $u -h localhost -p $p -w -d $db -c "COPY raw12Rec35 FROM '..\valuedata\2012\REC35_TRIM2012.CSV' DELIMITER ','"
+PSQL -U $u -h localhost -p $p -w -d $db -c "COPY raw12Rec36 FROM '..\valuedata\2012\REC36_COPY.CSV' DELIMITER ','"
+PSQL -U $u -h localhost -p $p -w -d $db -c "COPY raw12Rec40 FROM '..\valuedata\2012\REC40_TRIM2012.CSV' DELIMITER ','"
